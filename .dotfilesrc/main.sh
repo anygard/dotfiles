665a6c49010d71047a10063e82ffd853e1ee5471
Anders Nygård <anders.nygard@arbetsformedlingen.se>
2021-11-26 22:51:37 +0100

    Keep calm and Refactor

---
+++ .dotfilesrc/main.sh
@@ -0,0 +1,17 @@
+#
+#
+#	This file needs to be sourced to work
+#
+#
+
+if [ -n "$DOTFILESRC_RUNONLYONCE_GUARD" ]; then
+	return
+fi
+export DOTFILESRC_RUNONLYONCE_GUARD="There can be only one"
+
+
+echo
+echo "*** dotfilesrc ***"
+echo
+
+
