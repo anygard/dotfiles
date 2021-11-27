d5daa107e9575e7d1bcf40bed65f81159280357a
Anders Nygård <anders.nygard@arbetsformedlingen.se>
2021-11-27 14:07:03 +0100

    Improve installation procedure

--- install.sh
+++ install.sh
@@ -1,29 +1,32 @@
 #/usr/bin/env bash
+set -x
+BARENAME=.dotfiles
 
-$BARENAME=.dotfiles
+REPO="https://github.com/anygard/dotfiles.git"
+GITDIR="--git-dir=$HOME/$BARENAME"
+WORKTREE="--work-tree=$HOME"
 
-$REPO="https://github.com/anygard/dotfiles.git"
-$GITDIR="--git-dir=$HOME/$BARENAME"
-$WIRKTREE="--work-tree=$HOME"
-
-RCDIR=.dotfilesrc
+RCFILE=.dotfilesrc/main.sh
 
 function phase2 {
-	git config -f .gitconfig status.showUntrackedFiles no
 
-	git checkout $GITDIR $WORKTRE
+        # this is a workspace wide setting, e.g only for this
+        git --git-dir=$HOME/$BARENAME/ --work-tree=$HOME config status.showUntrackedFiles no
 
-	for target in ".bash_profile .bashrc" ; do
-	    
-	    cat "<<-EOP" >> $target
+        git $GITDIR $WORKTREE checkout
 
-	    # hook for .dotfiles
-	    if [ -f $RCDIR/entrypoint.sh ]; then
-		source .dotfilerc
-	    fi
-	    EOP
+        for target in .bash_profile .bashrc ; do
 
-	done
+                #just in case
+                cp $target ${target}.bak
+
+                # I do know about HERE DOCS but something in my syntax was wrong, will fix later
+                (echo
+                echo # hook for .dotfiles
+                echo "[ -f $RCFILE ] && source $RCFILE"
+                echo) >> $target
+
+        done
 
 }
 
@@ -32,46 +35,62 @@ echo "Prerequisite check"
 PREREQS="git"
 abort=false
 for pr in $PREREQS ; do
-	if [ -x $(which $pr) ]; then
-		echo "OK $pr"
-	else
-		abort=true
-	fi
-	if [ "$abort" == "true" ]; then
-                curl 
-		echo "One or more prerequisits were not met. Aborting."
-		echo
-		echo "Resolv the issues and try again"
-		exit 1
-	fi
+        if [ -x $(which $pr) ]; then
+                echo "OK $pr"
+        else
+                abort=true
+        fi
+        if [ "$abort" == "true" ]; then
 
-	if [ -e $BARENAME ]; then
-		echo "There already exists an entity called: ${BARENAME}. Aborting"
-		echo
-		echo "Resolv the issue and try again"
-		exit 3
-	fi
+                echo "One or more prerequisits were not met. Aborting."
+                echo
+                echo "Resolv the issues and try again"
+                exit 1
+        fi
+
+        if [ -e $BARENAME ]; then
+                echo "There already exists an entity called: ${BARENAME}. Aborting"
+                echo
+                echo "Resolv the issue and try again"
+                exit 3
+        fi
 done
 
-pushd $HOME
+echo "Installation"
+pushd $HOME > /dev/null
 
-if [ "$#" -ne 0 ]; then
+case $1 in
+        --online)
+                [ -n "$2" ] && REPO=$2
+                git clone --bare $REPO $BARENAME
+                ;;
+        --offline)
+                if [ -n "$2" -a -f "$2"]; then
+                        tar xzf $2
+                else
+                        echo "Required parameter missing or files does not exist"
+                        exit 2
+                fi
+                ;;
+        *)
+                echo "$(basename $0) < --online [repo url] | --offline <tar fname> >"
+                exit 2
+                ;;
+esac
 
-	# no parameters -> default online procedure
+phase2
 
-	git clone --bare $REPO $BARENAME
-	
-	phase2
+echo Installation complete
+echo
+echo run this command
+echo
+echo "        alias .f='/usr/bin/git --git-dir=$HOME/$BARENAME/ --work-tree=$HOME'"
+echo
+echo and then run
+echo
+echo "        .f checkout main"
+echo
+echo and logout and in again
 
-elif [ -n "$1" -a -f "$1" ]; then
-
-	# offline install
-	tar xzf $1 
-
-	phase2
-else
-	echo "No such file: $1"
-	exit 2
-fi
-
-popd
+popd > /dev/null
+set +x
