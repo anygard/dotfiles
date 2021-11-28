70aa02ee1fa6d603c2eded7b333f6e9028614d3a
Anders Nygård <anders.nygard@arbetsformedlingen.se>
2021-11-28 13:21:49 +0100

    WIP: UI stuff sorted, for now

--- .dotfilesrc/source.d/zz-prompt.sh
+++ .dotfilesrc/source.d/zz-prompt.sh
@@ -28,7 +28,7 @@ function git_info() {
         label=$1
 
 	tmp=$(mktemp)
-	git status --porcelain --branch > $tmp 2> /dev/null && return 0
+	git status --porcelain --branch > $tmp 2> /dev/null || return 0
 	header=$(head -1 $tmp)
 	tail=$(mktemp)
 	tail -n +2 $tmp > $tail
@@ -66,13 +66,22 @@ function render_preprompt() {
         #this must be first
         EC=$?
 
+	source $DOTFILESRC/source.d/00-config.sh
+
         TF=$(mktemp)
         git_info git >> $TF
         numjobs jobs >> $TF
         nonzero_return ec $EC >> $TF
         tmux
+        #set -x
         if [ -s "$TF" ]; then
-                echo
+                [ -n "$PROMPT_EXTRANL" -a "$PROMPT_EXTRANL" = Yes ] && echo
+                if [ -n "$PROMPT_COLOR" -a "$PROMPT_COLOR" = Yes ]; then
+			tput setaf 7
+			reset=$RESET
+		else
+			reset=
+		fi
                 cat $TF
                 rm $TF
                 echo
@@ -80,6 +89,7 @@ function render_preprompt() {
 	if [ -n "$ORIGINAL_PREPROMT_COMMAND" ]; then
 		$ORIGINAL_PREPROMT_COMMAND
 	fi
+        #set +x
 }
 
 # place code here that should only run on setup
@@ -87,5 +97,5 @@ function render_preprompt() {
 export ORIGINAL_PREPROMPT_COMMAND=$PREPROMPT_COMMAND
 PROMPT_COMMAND=render_preprompt
 #PS1="\e]2;\u2\h\a\A \u@\h \w \\$ "
-PS1="\A \u@\h \w \\$ "
+PS1="\A \u@\h \w \\$ $reset"
 
