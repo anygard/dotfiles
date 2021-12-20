45de570991fd70757a9c24c4ebe8174106a8ac11
Anders Nygård <anders.nygard@arbetsformedlingen.se>
2021-12-20 08:06:15 +0100

    Add som RHEL aliases

--- .dotfilesrc/source.d/alias.sh
+++ .dotfilesrc/source.d/alias.sh
@@ -5,3 +5,7 @@ alias .t='tmux attach'
 alias g=git
 alias vi=vim
 alias gpp="git pull --ff-only && git push"
+alias l.='ls -d .* --color=auto'
+alias ll='ls -l --color=auto'
+alias ls='ls --color=auto'
+
