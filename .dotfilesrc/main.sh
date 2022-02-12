ab920b3f6dd75639ae7641fa22cfbd1ea5c38057
Anders Nygård <anders.nygard@arbetsformedlingen.se>
2022-02-12 14:15:33 +0100

    Cleanup entry point

--- .dotfilesrc/main.sh
+++ .dotfilesrc/main.sh
@@ -4,22 +4,10 @@
 #
 #
 
-if [ -n "$DOTFILESRC_RUNONLYONCE_GUARD" ]; then
-	return
-fi
-export DOTFILESRC_RUNONLYONCE_GUARD="There can be only one"
-
-# I have to understand
 export LC_ALL="en_US.utf8"
 export LANG="en_US.utf8"
 
 export PATH=$PATH:$HOME/bin
-
-
-# temporary marker to visialize this is run
-#echo
-#echo "*** dotfilesrc ***"
-#echo
 
 # source .sh files in source.d
 pushd ~/.dotfilesrc/source.d > /dev/null
